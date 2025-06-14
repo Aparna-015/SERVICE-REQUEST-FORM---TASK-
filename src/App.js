d82b946106297bf8ed5166409b7cf9c5096ff46b
Aparna <aparnaraman995@gmail.com>
2025-06-14 12:57:09 +0530

    childdetails section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
+import "./App.css";
+import ChildDetails from "./Components/ChildeDetails/ChildDetails";
+import Contact from "./Components/Contact/Contact";
+import Service from "./Components/ServiceNeeds/Service";
 
 function App() {
   return (
-    <div className="App">
-    <h1>React</h1>
+    <div className="App bg-slate-200 h-screen flex items-center justify-center">
+      <div className="w-5/12  bg-white h-[600px] rounded-lg ">
+        <div className="w-full bg-blue-600 h-20 rounded">
+          <h1 className="text-white text-left font-bold p-4 text-2xl ">
+            Service request form
+          </h1>
+        </div>
+
+        <ChildDetails />
+        <Contact/>
+        <Service/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/ChildeDetails/ChildDetails.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ChildDetails = () => {
+  return (
+    <div className="main-container  text-left font-bold p-8">
+      <h2 className=" text-xl">Child Details</h2>
+      <div className=" text-left flex flex-col p-4">
+        <label className="font-normal">Child Age</label>
+        <input className="bg-slate-200" type="number" placeholder="Enter age" />
+      </div>
+      <div className=" text-left flex flex-col p-4">
+        <label className="font-normal">Diagnosis</label>
+        <input className="bg-slate-200" type="number" placeholder="eg: ADHD" />
+      </div>
+      <div className="P-4">
+        <label className="font-normal">Current school type</label>
+        <br />
+        <input type="radio" name="school type" value="Private" />
+        <label className="font-normal">Private</label> <br />
+        <input type="radio" name="school type" value="Public" />{" "}
+        <label className="font-normal">Public</label> <br />
+        <input type="radio" name="school type" value="SpeacialSchool" />
+        <label className="font-normal">Speacial</label>
+      </div>
+    </div>
+  );
+};
+
+export default ChildDetails;
